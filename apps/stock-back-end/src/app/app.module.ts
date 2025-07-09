08a13a231afa8fc8a81778247bd63d47fed07c3c
Brooot <xiangzhilong94wo@gmail.com>
2025-07-09 23:05:44 +0800

    feat: 加入validatePipe

--- apps/stock-back-end/src/app/app.module.ts
+++ apps/stock-back-end/src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe, // 直接使用内置管道
+    },
+  ],
 })
 export class AppModule {}
